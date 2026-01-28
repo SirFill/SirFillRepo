42743036d0cd571cf9686d0929e39c683edd81ec
SirFill <alexmarry02@gmail.com>
2026-01-28 01:36:05 +0100

    Update main URL from streamingunity.it to streamingunity.tv

--- StreamingCommunity/src/main/kotlin/it/dogior/hadEnough/StreamingCommunity.kt
+++ StreamingCommunity/src/main/kotlin/it/dogior/hadEnough/StreamingCommunity.kt
@@ -48,7 +48,7 @@ class StreamingCommunity(override var lang: String = "it") : MainAPI() {
             "X-Inertia-Version" to inertiaVersion,
             "X-Requested-With" to "XMLHttpRequest",
         ).toMutableMap()
-        val mainUrl = "https://streamingunity.it/"
+        val mainUrl = "https://streamingunity.tv/"
         var name = "StreamingCommunity"
         val TAG = "SCommunity"
     }
